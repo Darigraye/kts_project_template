ed8cc3484fba3ef84e0eb8a572158fe1f7e19a37
Eugene <eugene.shpachkov@gmail.com>
2023-03-20 12:56:32 +0300

    add alembic and connection to database with creting tables

--- kts_backend/store/__init__.py
+++ kts_backend/store/__init__.py
@@ -1,5 +1,26 @@
-class Store:
-    def __init__(self, *args, **kwargs):
-        from kts_backend.users.accessor import UserAccessor
+from typing import TYPE_CHECKING
 
-        self.user = UserAccessor(self)
+from kts_backend.store.database import Database
+from kts_backend.users.models import *
+from kts_backend.games.models import *
+
+if TYPE_CHECKING:
+    from kts_backend.web.app import Application
+
+
+class Store:
+    def __init__(self, app: "Application",  *args, **kwargs):
+        from kts_backend.users.accessor import UserAccessor
+        from kts_backend.store.vk_api.accessor import VkApiAccessor
+        from kts_backend.games.accessor import GameAccessor
+
+        self.vk_api = VkApiAccessor(app)
+        self.user = UserAccessor(app)
+        self.game = GameAccessor(app)
+
+
+def setup_store(app: "Application"):
+    app.database = Database(app)
+    app.on_startup.append(app.database.connect)
+    app.on_cleanup.append(app.database.disconnect)
+    app.store = Store(app)
---
+++ kts_backend/store/database/database.py
@@ -0,0 +1,34 @@
+from typing import Optional, TYPE_CHECKING
+
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from kts_backend.store.database.sqlalchemy_database import db
+
+if TYPE_CHECKING:
+    from kts_backend.web.app import Application
+
+
+class Database:
+    def __init__(self, app: "Application"):
+        self.app = app
+        self._engine: Optional[AsyncEngine] = None
+        self._db: Optional[declarative_base] = None
+        self.session: Optional[AsyncSession] = None
+
+    @staticmethod
+    def _build_url_to_connect(username: str, password: str, database: str, host: str = 'localhost') -> str:
+        return f'postgresql+asyncpg://{username}:{password}@{host}/{database}'
+
+    async def connect(self, *args, **kwargs) -> None:
+        self._db = db
+        url_to_connect: str = self._build_url_to_connect(username=self.app.config.database.user,
+                                                         password=self.app.config.database.password,
+                                                         database=self.app.config.database.database)
+        self._engine = create_async_engine(url_to_connect,
+                                           echo=True,
+                                           future=True)
+        self.session = sessionmaker(self._engine, expire_on_commit=False, future=True, class_=AsyncSession)
+
+    async def disconnect(self, *args, **kwargs) -> None:
+        if self._engine:
+            await self._engine.dispose()
